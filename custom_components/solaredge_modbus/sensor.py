4d23edabe4d0298a4b3dffb1053d90fdb31b9599
Sander Revenberg <sander.revenberg@ing.com>
2023-05-10 19:52:50 +0200

    lint

--- custom_components/solaredge_modbus/sensor.py
+++ custom_components/solaredge_modbus/sensor.py
@@ -1,10 +1,11 @@
+'''Sensor SolarEdge.'''
 import datetime
 import asyncio
 import traceback
 
-from time import sleep
+#from time import sleep
 
-from datetime import timedelta
+#from datetime import timedelta
 import logging
 
 from homeassistant.const import CONF_SCAN_INTERVAL
@@ -25,6 +26,7 @@ values = {}
 meter1_values = {}
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    '''Async_setup_platform.'''
     if discovery_info is None:
         return
 
@@ -36,7 +38,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 
 class SolarEdgeModbusSensor(Entity):
+    '''SolarEdgeModbusSensor.'''
     def __init__(self, instrument, scan_interval):
+        '''Init.'''
         _LOGGER.debug("creating modbus sensor")
         #self.entity_id = generate_entity_id("sensor.{}", "SolarEdgeModbusSensor")
 
@@ -46,14 +50,17 @@ class SolarEdgeModbusSensor(Entity):
         self._device_state_attributes = {}
 
     def getValueLong(self, addr, numberOfDecimals=0, functioncode=0, signed=False):
+        '''GetValueLong.'''
         rc = self._instrument.read_long(addr, functioncode=functioncode, signed=signed)
         return rc
 
     def getValueRegister(self, addr, numberOfDecimals=0, functioncode=0, signed=False):
+        '''GetValueRegister.'''
         rc = self._instrument.read_register(addr, numberOfDecimals=numberOfDecimals, functioncode=functioncode, signed=signed)
         return rc
 
     def round(self, floatval):
+        '''Round.'''
         return round(floatval, 2)
 
     @property
@@ -67,11 +74,13 @@ class SolarEdgeModbusSensor(Entity):
         return self._state
 
     async def async_added_to_hass(self):
+        '''Async_added_to_hass.'''
         _LOGGER.debug("added to hass, starting loop")
         loop = self.hass.loop
         task = loop.create_task(self.modbus_loop())
 
     async def modbus_loop(self):
+        '''Modbus_loop.'''
         _LOGGER.debug("modbus_loop")
         while True:
             try:
