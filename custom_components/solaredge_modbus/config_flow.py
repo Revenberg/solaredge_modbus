b2c3aefb787cd1add969620f673611b52a7288a1
Sander Revenberg <sander.revenberg@ing.com>
2023-05-13 13:36:58 +0200

    new

--- custom_components/solaredge_modbus/config_flow.py
+++ custom_components/solaredge_modbus/config_flow.py
@@ -1,140 +1,77 @@
-"""solarEdge_modbus."""
-from __future__ import annotations
-import logging
-from typing import Any
+import ipaddress
+import re
 
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import (CONF_HOST, CONF_NAME, CONF_PORT,
+                                 CONF_SCAN_INTERVAL)
+from homeassistant.core import HomeAssistant, callback
 
-from homeassistant import config_entries, exceptions
-from homeassistant.core import HomeAssistant
+from .const import (
+	DEFAULT_NAME,
+	DEFAULT_PORT,
+	DEFAULT_SCAN_INTERVAL,
+	DOMAIN,
+)
 
-#from homeassistant.const import CONF_ACCESS_TOKEN, CONF_NAME, CONF_PATH, CONF_URL, CONF_TYPE,
-from homeassistant.const import CONF_DEVICE
-from .const import DOMAIN
-
-#from .hub import Hub
-#import serial
-import os
-
-_LOGGER = logging.getLogger(__name__)
-
-# This is the schema that used to display the UI to the user. This simple
-# schema has a single required host field, but it could include a number of fields
-# such as username, password etc. See other components in the HA core code for
-# further examples.
-# Note the input displayed to the user will be translated. See the
-# translations/<lang>.json file and strings.json. See here for further information:
-# https://developers.home-assistant.io/docs/config_entries_config_flow_handler/#translations
-# At the time of writing I found the translations created by the scaffold didn't
-# quite work as documented and always gave me the "Lokalise key references" string
-# (in square brackets), rather than the actual translated value. I did not attempt to
-# figure this out or look further into it.
-DATA_SCHEMA = vol.Schema({("host"): str, ("port"): str})
-
-def get_serial_by_id(dev_path: str) -> str:
-    """Return a /dev/serial/by-id match for given device if available."""
-    by_id = "/dev/serial/by-id"
-    if not os.path.isdir(by_id):
-        return dev_path
-
-    for path in (entry.path for entry in os.scandir(by_id) if entry.is_symlink()):
-        if os.path.realpath(path) == dev_path:
-            return path
-    return dev_path
-
-async def validate_input_title(hass: HomeAssistant, data: dict) -> dict[str, Any]:
-    """Validate the user input allows us to connect.
-
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
-    # Validate the data can be used to set up a connection.
-
-    # This is a simple example to show an error in the UI for a short hostname
-    # The exceptions are defined at the end of this file, and are used in the
-    # `async_step_user` method below.
-    if len(data["host"]) < 3:
-        raise InvalidHost
-
-    return {"title": data["host"]}
-
-async def validate_input_device(hass: HomeAssistant, data: dict) -> dict[str, Any]:
-    """Validate the user input device to connect."""
-    if len(data[CONF_DEVICE ]) < 3:
-        raise InvalidHost
-
-#    hub = Hub(hass, data["title"], data[ CONF_DEVICE ])
-    # The dummy hub provides a `test_connection` method to ensure it's working
-    # as expected
-#    result = await hub.connection()
-#    if not result:
-        # If there is an error, raise an exception to notify HA that there was a
-        # problem. The UI will also show there was a problem
-#        raise CannotConnect
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    # "Title" is what is displayed to the user for this hub device
-    # It is stored internally in HA as part of the device config.
-    # See `async_step_user` below for how this is used
-    return {"title": data["title"], "device": data[ CONF_DEVICE ]}
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Required(CONF_HOST): str,
+        vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
+    }
+)
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Hello World."""
+def host_valid(host):
+    """Return True if hostname or IP address is valid."""
+    try:
+        if ipaddress.ip_address(host).version == (4 or 6):
+            return True
+    except ValueError:
+        disallowed = re.compile(r"[^a-zA-Z\d\-]")
+        return all(x and not disallowed.search(x) for x in host.split("."))
+
+
+@callback
+def iammeter_modbus_entries(hass: HomeAssistant):
+    """Return the hosts already configured."""
+    return set(
+        entry.data[CONF_HOST] for entry in hass.config_entries.async_entries(DOMAIN)
+    )
+
+
+class IammeterModbusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Iammeter Modbus configflow."""
 
     VERSION = 1
-    # Pick one of the available connection classes in homeassistant/config_entries.py
-    # This tells HA if it should be asking for updates, or it'll be notified of updates
-    # automatically. This example uses PUSH, as the dummy hub will notify HA of
-    # changes.
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    #data: Optional[Dict[str, Any]]
+    def _host_in_configuration_exists(self, host) -> bool:
+        """Return True if host exists in configuration."""
+        if host in iammeter_modbus_entries(self.hass):
+            return True
+        return False
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        # This goes through the steps to take the user through the setup process.
-        # Using this it is possible to update the UI and prompt for additional
-        # information. This example provides a single form (built from `DATA_SCHEMA`),
-        # and when that has some validated input, it calls `async_create_entry` to
-        # actually create the HA config entry. Note the "title" value is returned by
-        # `validate_input` above.
         errors = {}
-        if user_input is not None:
-            try:
-                self.data = await validate_input_title(self.hass, user_input )
-#                return await self.async_step_setup_serial()
-                return self.async_create_entry(title="solaredge_modbus", data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidHost:
-                # The error string is set here, and should be translated.
-                # This example does not currently cover translations, see the
-                # comments on `DATA_SCHEMA` for further details.
-                # Set the error on the `host` field, not the entire form.
-                errors["host"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
 
-        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
+        if user_input is not None:
+            host = user_input[CONF_HOST]
+
+            if self._host_in_configuration_exists(host):
+                errors[CONF_HOST] = "already_configured"
+            elif not host_valid(user_input[CONF_HOST]):
+                errors[CONF_HOST] = "invalid host IP"
+            else:
+                await self.async_set_unique_id(user_input[CONF_HOST])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=user_input[CONF_NAME], data=user_input
+                )
+
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate there is an invalid hostname."""
--- custom_components/solaredge_modbus/sensor.py
+++ custom_components/solaredge_modbus/sensor.py
@@ -1,207 +1,87 @@
-"""Sensor SolarEdge."""
-#import datetime
-import asyncio
-#import traceback
-
-#from time import sleep
-
-from datetime import timedelta
+from homeassistant.const import CONF_NAME
+from homeassistant.core import callback
+from homeassistant.components.sensor import SensorEntity
 import logging
+from typing import Optional, Dict, Any
 
-#from homeassistant.const import CONF_SCAN_INTERVAL
 
-from homeassistant.helpers.entity import Entity
-from homeassistant import config_entries, core
+import homeassistant.util.dt as dt_util
 
-from . import DOMAIN
-#from . import CONF_SCAN_INTERVAL
-
-#from homeassistant.helpers.entity import generate_entity_id
+from .const import ATTR_MANUFACTURER, DOMAIN, SENSOR_TYPES, IamMeterModbusSensorEntityDescription
 
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
 
-ICON = "mdi:power-plug"
-SCAN_INTERVAL = timedelta(seconds=60)
 
-values = {}
-meter1_values = {}
+async def async_setup_entry(hass, entry, async_add_entities):
+    hub_name = entry.data[CONF_NAME]
+    hub = hass.data[DOMAIN][hub_name]["hub"]
 
-async def async_setup_entry(hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry, async_add_entities):
-    """Add sensors for passed config_entry in HA."""
-    _LOGGER.debug("async_setup_entry")
-    hub = hass.data[DOMAIN][config_entry.entry_id]
+    device_info = {
+        "identifiers": {(DOMAIN, hub_name)},
+        "name": hub_name,
+        "manufacturer": ATTR_MANUFACTURER,
+    }
 
-    _LOGGER.debug("fetching modbus client")
+    entities = []
+    for sensor_description in SENSOR_TYPES.values():
+        sensor = IamMeterModbusSensor(
+            hub_name,
+            hub,
+            device_info,
+            sensor_description,
+        )
+        entities.append(sensor)
 
-#    _LOGGER.debug("fetching modbus client")
-#    instrument = hass.data.get(DOMAIN)
-#    scan_interval = discovery_info[CONF_SCAN_INTERVAL]
+    async_add_entities(entities)
+    return True
 
-#    device = hass.data.get( CONF_DEVICE )
-#    _LOGGER.debug(  device  )
-    title =  hass.data.get( 'title' )
-    _LOGGER.debug(  title  )
-    _LOGGER.debug("---------------------------------------------------")
-    _LOGGER.debug(hub._instrument)
-    _LOGGER.debug("---------------------------------------------------")
 
-    timedelta(seconds=60)
+class IamMeterModbusSensor(SensorEntity):
+    """Representation of an IamMeter Modbus sensor."""
 
-#    async_add_entities([energySensor1(hub._instrument, scan_interval)], True)
-    async_add_entities([SolarEdgeModbusSensor( hub._instrument, 60)], True)
-
-class SolarEdgeModbusSensor(Entity):
-    """Solar EdgeModbus Sensor."""
-
-    def __init__(self, instrument, scan_interval):
-        """Init."""
-        _LOGGER.debug("creating modbus sensor")
-        #self.entity_id = generate_entity_id("sensor.{}", "SolarEdgeModbusSensor")
-
-        self._instrument = instrument
-        self._scan_interval = scan_interval
-        self._state = 0
-        self._device_state_attributes = {}
-
-    def getValueLong(self, addr, numberOfDecimals=0, functioncode=0, signed=False):
-        """GetValueLong."""
-        rc = self._instrument.read_long(addr, functioncode=functioncode, signed=signed)
-        return rc
-
-    def getValueRegister(self, addr, numberOfDecimals=0, functioncode=0, signed=False):
-        """GetValueRegister."""
-        rc = self._instrument.read_register(addr, numberOfDecimals=numberOfDecimals, functioncode=functioncode, signed=signed)
-        return rc
-
-    def round(self, floatval):
-        """Round."""
-        return round(floatval, 2)
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return self._device_state_attributes
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
+    def __init__(
+        self,
+        platform_name,
+        hub,
+        device_info,
+        description: IamMeterModbusSensorEntityDescription,
+    ):
+        """Initialize the sensor."""
+        self._platform_name = platform_name
+        self._attr_device_info = device_info
+        self._hub = hub
+        self.entity_description: IamMeterModbusSensorEntityDescription = description
 
     async def async_added_to_hass(self):
-        """Async_added_to_hass."""
-        _LOGGER.debug("added to hass, starting loop")
-        loop = self.hass.loop
-        #task =
-        loop.create_task(self.modbus_loop())
+        """Register callbacks."""
+        self._hub.async_add_iammeter_modbus_sensor(self._modbus_data_updated)
 
-    async def modbus_loop(self):
-        """Modbus_loop."""
-        _LOGGER.debug("modbus_loop")
-        while True:
-            try:
-                values['ac_lifetimeproduction'] = self.getValueLong(3008, functioncode=4, signed=False) # Read All Time Energy (KWH Total) as Unsigned 32-Bit
-                values['generatedtoday'] = self.getValueRegister(3014, numberOfDecimals=1, functioncode=4, signed=False) # Read Today Energy (KWH Total) as 16-Bit
-                values['generatedyesterday'] = self.getValueRegister(3015, numberOfDecimals=1, functioncode=4, signed=False) # Read Today Energy (KWH Total) as 16-Bit
-                values['ac_power_output'] = self.getValueLong(3004, functioncode=4, signed=False)
+    async def async_will_remove_from_hass(self) -> None:
+        self._hub.async_remove_iammeter_modbus_sensor(self._modbus_data_updated)
 
-                values['dc_voltage1'] = self.getValueRegister(3021, numberOfDecimals=1, functioncode=4, signed=False)
-                values['dc_current1'] = self.getValueRegister(3022, functioncode=4, signed=False)
-                values['dc_voltage2'] = self.getValueRegister(3023, numberOfDecimals=1, functioncode=4, signed=False)
-                values['dc_current2'] = self.getValueRegister(3024, functioncode=4, signed=False)
+    @callback
+    def _modbus_data_updated(self):
+        self.async_write_ha_state()
 
-                values['ac_voltage_phase_a'] = self.getValueRegister(3033, numberOfDecimals=1,functioncode=4, signed=False)
-                values['ac_voltage_phase_b'] = self.getValueRegister(3034, numberOfDecimals=1, functioncode=4, signed=False)
-                values['ac_voltage_phase_c'] = self.getValueRegister(3035, numberOfDecimals=1, functioncode=4, signed=False)
-
-                values['ac_current_phase_a'] = self.getValueRegister(3036, numberOfDecimals=1, functioncode=4, signed=False)
-                values['ac_current_phase_b'] = self.getValueRegister(3037, numberOfDecimals=1, functioncode=4, signed=False)
-                values['ac_current_phase_c'] = self.getValueRegister(3038, numberOfDecimals=1, functioncode=4, signed=False)
-
-                values['ac_frequency'] = self.getValueRegister(3042, numberOfDecimals=2, functioncode=4, signed=False)
-                values['heat_sink_temperature'] = self.getValueRegister(3041, numberOfDecimals=1, functioncode=4, signed=True)
-
-                Realtime_DATA_yy = self.getValueRegister(3072, functioncode=4, signed=False) #Read Year
-                Realtime_DATA_mm = self.getValueRegister(3073, functioncode=4, signed=False) #Read Month
-                Realtime_DATA_dd = self.getValueRegister(3074, functioncode=4, signed=False) #Read Day
-                Realtime_DATA_hh = self.getValueRegister(3075, functioncode=4, signed=False) #Read Hour
-                Realtime_DATA_mi = self.getValueRegister(3076, functioncode=4, signed=False) #Read Minute
-                Realtime_DATA_ss = self.getValueRegister(3077, functioncode=4, signed=False) #Read Second
-
-                tms = "20" + str(Realtime_DATA_yy) + "-" + str(Realtime_DATA_mm) + "-" + str(Realtime_DATA_dd)
-                tms = tms + " " + str(Realtime_DATA_hh) + ":" + str(Realtime_DATA_mi) + ':' + str(Realtime_DATA_ss)
-                values['timestamp'] = tms
-
-                values['ac_total_current'] = self.getValueRegister(3005, functioncode=4, signed=False)
-                values['pvpower'] = self.getValueRegister(3007, functioncode=4, signed=False)
-                values['ac_totalenergy'] = self.getValueRegister(3009, functioncode=4, signed=False)
-                values['ac_monthenergy'] = self.getValueRegister(3011, functioncode=4, signed=False)
-                values['ac_lastmonth'] = self.getValueRegister(3013, functioncode=4, signed=False)
-                values['ac_yearenergy'] = self.getValueRegister(3017, functioncode=4, signed=False)
-                values['ac_lastyear'] = self.getValueRegister(3019, functioncode=4, signed=False)
-
-                #debug-print entire dictionary
-                #for x in values.keys():
-                #    _LOGGER.debug(x +" => " + str(values[x]))
-
-                # Skip if increamenting counter has gone to 0, or become too large, this happens on inverter startup
-
-                validValue = values['ac_lifetimeproduction'] > 0
-
-                if validValue:
-                    #main state is power production, other values can be fetched as attributes
-                    self._state = 4
-                    values['_state'] = self._state
-                    self._device_state_attributes = values
-
-            except TimeoutError:
-              _LOGGER.debug("Timeout")
-              self._state = 2
-              values['_state'] = self._state
-              self._device_state_attributes = values
-              _LOGGER.debug( values )
-            except Exception as e:
-                self.status = 7
-                _LOGGER.error(f'exception: {e}')
-                #print(traceback.format_exc())
-                values['_state'] = self._state
-                self._device_state_attributes = values
-
-            _LOGGER.debug( "_state: " + str(self._state) )
-
-            # tell HA there is new data
-            self.async_schedule_update_ha_state()
-            await asyncio.sleep(self._scan_interval)
+    @callback
+    def _update_state(self):
+        if self._key in self._hub.data:
+            self._state = self._hub.data[self._key]
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return "SolarEdge Modbus"
+        """Return the name."""
+        return f"{self._platform_name} {self.entity_description.name}"
 
     @property
-    def should_poll(self):
-        """Return the polling state."""
-        return False
-
+    def unique_id(self) -> Optional[str]:
+        return f"{self._platform_name}_{self.entity_description.key}"  
+    
     @property
-    def icon(self):
-        """Return the icon to use in the frontend."""
-        return ICON
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity."""
-        return "W"
-
-    @property
-    def unique_id(self):
-        """Unique Id."""
-        _LOGGER.debug("unique_id")
-        #_LOGGER.debug(self._instrument)
-
-        #_LOGGER.debug(self._instrument.eth_address)
-        #_LOGGER.debug(self._instrument.eth_port)
-
-#        _LOGGER.debug(f"{self._instrument.eth_address}:{self._instrument.eth_port}")
-#        return f"{self._instrument.eth_address}:{self._instrument.eth_port}"
-        return "SolarEdge"
+    def native_value(self):
+        """Return the state of the sensor."""
+        return (
+        	self._hub.data[self.entity_description.key]
+        	if self.entity_description.key in self._hub.data
+        	else None
+        )
