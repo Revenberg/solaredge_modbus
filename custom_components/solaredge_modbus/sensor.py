db7c0580b7213013583dd1e874beb5ed029d4841
Sander Revenberg <sander.revenberg@ing.com>
2023-05-24 21:15:58 +0200

    .

--- custom_components/solaredge_modbus/sensor.py
+++ custom_components/solaredge_modbus/sensor.py
@@ -8,7 +8,7 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
-    PERCENTAGE,
+#    PERCENTAGE,
     POWER_VOLT_AMPERE_REACTIVE,
     UnitOfApparentPower,
     UnitOfElectricCurrent,
